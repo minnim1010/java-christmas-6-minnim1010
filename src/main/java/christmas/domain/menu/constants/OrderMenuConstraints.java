df55c81aa4d49a39c7bf85f8acbb8c6648ab4471
minnim1010 <pms1010123@daum.net>
2023-11-17 01:14:06 +0900

    refactor(OrderMenuConstraints): 필드 접근 제어 방식 수정

--- src/main/java/christmas/domain/menu/constants/OrderMenuConstraints.java
+++ src/main/java/christmas/domain/menu/constants/OrderMenuConstraints.java
@@ -1,10 +1,10 @@
 package christmas.domain.menu.constants;
 
 public class OrderMenuConstraints {
-    public static final int MIN_ORDER_MENU_COUNT = 1;
-    public static final int MAX_ORDER_MENU_COUNT = 20;
-    public static final int MIN_TOTAL_ORDER_MENU_COUNT = 1;
-    public static final int MAX_TOTAL_ORDER_MENU_COUNT = 20;
+    private static final int MIN_ORDER_MENU_COUNT = 1;
+    private static final int MAX_ORDER_MENU_COUNT = 20;
+    private static final int MIN_TOTAL_ORDER_MENU_COUNT = 1;
+    private static final int MAX_TOTAL_ORDER_MENU_COUNT = 20;
 
     private OrderMenuConstraints() {
     }
